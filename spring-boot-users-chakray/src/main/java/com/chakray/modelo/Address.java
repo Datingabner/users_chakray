8cb1872cdc71eac3cafbfdc2071d236a9d1ddfb7
Abner Isaí Juárez Hernández <abner23_08@hotmail.com>
2025-06-26 19:10:38 -0600

    Se agregaron los modelos para la conexion o relacion a la base de datos.

---
+++ spring-boot-users-chakray/src/main/java/com/chakray/modelo/Address.java
@@ -0,0 +1,102 @@
+package com.chakray.modelo;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "address")
+public class Address {
+	
+	
+	@Id
+	@Column(name = "id", nullable = false)
+	@GeneratedValue(generator = "increment")
+	private int id;
+	
+	@Column(name = "name", nullable = false)
+	private String name;
+	
+	@Column(name = "street", nullable = false)
+	private String street;
+	
+	@Column(name = "country_code", nullable = false)
+	private String country_code;
+	
+	
+	@OneToOne
+	@JoinColumn(name = "related_user", referencedColumnName = "id", nullable = false)
+	private User related_user;
+
+
+	public Address() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+
+	public Address(int id, String name, String street, String country_code, User related_user) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.street = street;
+		this.country_code = country_code;
+		this.related_user = related_user;
+	}
+
+
+	public int getId() {
+		return id;
+	}
+
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+
+	public String getName() {
+		return name;
+	}
+
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+
+	public String getStreet() {
+		return street;
+	}
+
+
+	public void setStreet(String street) {
+		this.street = street;
+	}
+
+
+	public String getCountry_code() {
+		return country_code;
+	}
+
+
+	public void setCountry_code(String country_code) {
+		this.country_code = country_code;
+	}
+
+
+	public User getRelated_user() {
+		return related_user;
+	}
+
+
+	public void setRelated_user(User related_user) {
+		this.related_user = related_user;
+	}
+	
+	
+}
---
+++ spring-boot-users-chakray/src/main/java/com/chakray/modelo/User.java
@@ -0,0 +1,88 @@
+package com.chakray.modelo;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "Users_chakray")
+public class User {
+	
+	
+	@Id
+	@Column(name ="id", nullable = false)
+	@GeneratedValue(generator = "increment")
+	private int id;
+	
+	@Column(name = "email", nullable = false)
+	private String email;
+	
+	@Column(name="name", nullable = false)
+	private String name;
+	
+	@Column(name = "password", nullable = false)
+	private String password;
+	
+	@Column(name = "created_at", nullable = false)
+	private String created_at;
+
+	public User() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public User(int id, String email, String name, String password, String created_at) {
+		super();
+		this.id = id;
+		this.email = email;
+		this.name = name;
+		this.password = password;
+		this.created_at = created_at;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getCreated_at() {
+		return created_at;
+	}
+
+	public void setCreated_at(String created_at) {
+		this.created_at = created_at;
+	}
+	
+	
+	
+	
+}

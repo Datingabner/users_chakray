e708fee4598465230c5bbaa2d2cf1c10dfe4358b
Abner Isaí Juárez Hernández <abner23_08@hotmail.com>
2025-06-26 18:43:15 -0600

    Se creo el proyecto (todo está por default)

---
+++ spring-boot-users-chakray/src/main/java/com/chakray/SpringBootUsersChakrayApplication.java
@@ -0,0 +1,13 @@
+package com.chakray;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootUsersChakrayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootUsersChakrayApplication.class, args);
+	}
+
+}
---
+++ spring-boot-users-chakray/src/test/java/com/chakray/SpringBootUsersChakrayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chakray;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootUsersChakrayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
